c732d96b2b046a850ed308d9f2b815d492d8a2db
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-09-09 13:24:52 -0300

    funções

---
+++ funcion.py
@@ -0,0 +1,7 @@
+import os
+
+def menu():
+  print("1-Cadastro cliente \n2-Cadastro de jogo  \n3-Cadastro de filme \n4-Listar tudo \n5-Listar jogos \n6-Listar filmes \n7-Alugar filme \n8-Alugar jogo \n9-Devolver filme \n10-Devolver jogo \n11-Sair")
+
+def ls():
+  os.system("pause")
