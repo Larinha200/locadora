3b0ae2efebd35146c806251f91bfc95fe3edea3f
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-09-09 10:30:00 -0300

    classe itens

---
+++ claases.py
@@ -0,0 +1,16 @@
+class Itens:
+    def __init__(self,Id,Titulo,Disponivel):
+        self.__Id= Id
+        self.__Titulo = Titulo
+        self.__Disponivel = Disponivel
+
+    
+    def Alugar():
+        pass
+
+    def Devolver():
+        pass
+
+
+    
+
