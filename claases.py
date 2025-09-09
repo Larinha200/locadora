048a0eabefc828fb6f684fbc97ddfaccb3af6e2f
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-09-09 10:53:45 -0300

    classe locadora e concertei alguns erros

--- claases.py
+++ claases.py
@@ -1,5 +1,5 @@
 class Itens:
-    def __init__(self,id,titulo,disponivel):
+    def __init__(self, id:int, titulo:str, disponivel:bool):
         self.__id= id
         self.__titulo = titulo
         self.__disponivel = disponivel
@@ -12,21 +12,21 @@ class Itens:
         pass
 
 class Filmes(Itens):
-    def __init__(self,id, titulo,genero,duração):
+    def __init__(self, id:int, titulo:str, genero:str, duração:int):
         self.__id= id
         self.__titulo = titulo
         self.__genero = genero
         self.__duração = duração
     
 class Jogos(Itens):
-    def __init__(self,id, titulo,plataforma,faixa_etaria):
+    def __init__(self, id:int, titulo:str, plataforma:str, faixa_etaria:int):
         self.__id= id
         self.__titulo = titulo
         self.__plataforma = plataforma
         self.__faixa_etaria = faixa_etaria
 
 class Clientes:
-    def __init__(self,nome, cpf, itens_locados):
+    def __init__(self,nome:str, cpf:str, itens_locados:list):
         self.__nome = nome
         self.__cpf = cpf
         self.__itens_locados = itens_locados
@@ -40,3 +40,19 @@ class Clientes:
     def Listar_itens():
         pass
 
+class Locadora:
+    def __init__(self, clientes:list, itens:list):
+        self.__clientes = clientes
+        self.__itens = itens
+
+    def Cadastrar_clientes():
+        pass
+
+    def Cadastrar_item():
+        pass
+
+    def Listar_clientes():
+        pass
+
+    def Lista_itens():
+        pass
