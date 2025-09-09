5206d5a2981ae969ee906eb4cfe87a21a772c9ac
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-09-09 13:37:57 -0300

    match case criado

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
 from funcion import*
+
+menu()
+resp=("Digite: \n --->")
+
+match resp:
+    case 1:
+        pass
+    case 2:
+        pass
+    case 3:
+        pass
+    case 4:
+        pass
+    case 5:
+        pass
+    case 6:
+        pass
+    case 7:
+        pass
+    case 8:
+        pass
+    case 9:
+        pass
+    case 10:
+        pass
+    case 11:
+        pass
+    case _:
+        pass
