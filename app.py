6f5a70230e2efa63402b663d5ab26c1681c3b25c
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-09-16 07:57:59 -0300

    correção2

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ resp=int(input("Digite: --->"))
 while True:
     match resp:
         case 1:
-            list_cli.Cadastrar_clientes()
+            Locadora.Cadastrar_clientes()
             menu()
             resp=int(input("Digite: --->"))
            
